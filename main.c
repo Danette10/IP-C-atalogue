da58b6d5d7390f66bb5641b91cc7968aaeccc141
Dan Sebag <dsebag@asptt.com>
2023-09-26 22:52:22 +0200

    Add functions to add new ip addresses and verify if ip is valid and save this in the file

--- main.c
+++ main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include "headers/ipProcessing.h"
 
 int main() {
-    printf("Hello, World!\n");
+    char choice;
+
+    while (1) {
+        printf("\n\n");
+        printf(" ::::::::                   ::: ::::::::::: :::     :::        ::::::::   ::::::::  :::    ::: ::::::::::\n");
+        printf(":+:    :+:                :+: :+:   :+:   :+: :+:   :+:       :+:    :+: :+:    :+: :+:    :+: :+:    :+: \n");
+        printf("+:+                      +:+   +:+  +:+  +:+   +:+  +:+       +:+    +:+ +:+        +:+    +:+ +:+\n");
+        printf("+#+++   +#++:++#++:++ +#++:++#++: +#+ +#++:++#++: +#+       +#+    +:+ :#:        +#+    +:+ +#++:++#\n");
+        printf("+#+                     +#+     +#+ +#+ +#+     +#+ +#+       +#+    +#+ +#+   +#+# +#+    +#+ +#+\n");
+        printf("#+#    #+#              #+#     #+# #+# #+#     #+# #+#       #+#    #+# #+#    #+# #+#    #+# #+#\n");
+        printf(" ########               ###     ### ### ###     ### ########## ########   ########   ########  ##########\n");
+        printf("a - Add a new IP address\n");
+        printf("l - List IP addresses\n");
+        printf("s - Search similar by mask\n");
+        printf("d - Delete an IP\n");
+        printf("q - Quit\n");
+        printf("Choice : \n");
+        scanf(" %c", &choice);
+
+        switch (choice) {
+            case 'a': {
+                ip_addr ip;
+                ip_addr mask;
+                printf("Enter address : \n");
+                scanf("%d.%d.%d.%d", &ip.octets[0], &ip.octets[1], &ip.octets[2], &ip.octets[3]);
+                printf("Enter mask : \n");
+                scanf("%d.%d.%d.%d", &mask.octets[0], &mask.octets[1], &mask.octets[2], &mask.octets[3]);
+                add_ip(ip);
+                add_mask(mask);
+                break;
+            }
+            case 'q': {
+                return 0;
+            }
+            default: {
+                printf("Invalid choice\n");
+                break;
+            }
+        }
+    }
     return 0;
 }
